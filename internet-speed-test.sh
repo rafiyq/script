76da6619e692eac3864109c9fd1137c8bd5c5f9a
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-10-15 08:39:22 +0700

    Internet-speed-test: modified file path

--- internet-speed-test.sh
+++ internet-speed-test.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-save_to="$HOME/Documents/internet-speed-test"
-temp_file="$HOME/Documents/internet-speed-temp"
+save_to="$HOME/Documents/logs/internet-speed-test.txt"
+temp_file="$HOME/Documents/logs/internet-speed-temp"
 interval=600
 
-[ ! -f $save_to ] && echo "Date|Time|Ping|Download|Upload" > $save_to
+[ ! -f $save_to ] && echo "Date|Time|Wireles|IP Address|Ping|Download|Upload" > $save_to
 while true
 do
     date +%Y-%m-%d > $temp_file
     date | awk '{print $5}' >> $temp_file
-    speedtest-cli --simple | awk '{print $2,$3}' >>$temp_file
+    nmcli | grep -m2 "connected to\|inet4" | awk '{print $NF}' >> $temp_file
+    speedtest-cli --simple | awk '{print $2,$3}' >> $temp_file
     sed ':a;N;$!ba;s/\n/\|/g' $temp_file >> $save_to
     rm $temp_file
     sleep $interval
