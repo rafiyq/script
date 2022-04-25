f1cebb01ac9ff4551e773eea94733570529df7bc
rafiyq <rafiyq@protonmail.com>
2022-04-26 04:43:36 +0700

    merge emacs profiler

--- emacs-profile.sh
+++ emacs-profile.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+spacemacs_url="https://github.com/syl20bnr/spacemacs"
+spacemacs_path="$HOME/.config/spacemacs"
+
+# clone spacemacs
+[ ! -d $spacemacs_path/master ] && git clone -b master $spacemacs_url $spacemacs_path/master
+[ ! -d $spacemacs_path/develop ] && git clone -b develop $spacemacs_url $spacemacs_path/develop    
+    
 rm -rf ~/.emacs.d
 case $1 in
     "emacs")
