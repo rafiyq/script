344ff5bd7990c7bd41cc8454c53d0a94113b819a
rafiyq <rafiyq@protonmail.com>
2022-05-13 14:24:16 +0700

    create function to clone theme

--- zsh-profile.sh
+++ zsh-profile.sh
@@ -10,17 +10,30 @@ fi
 
 #prefix=$(dirname $(readlink -f $0))
 #[ ! -z "$1" ] && prefix=$1 
-newclone ()
+newplug ()
 {
     plug_url=$1
     plug_name=${plug_url##*/}
     plug_dir=$zsh_dir/plugins/$plug_name
-    if [ ! -d $plug_dir ]
-    then
+    if [ ! -d $plug_dir ]; then
         git clone $plug_url $plug_dir
     else
         echo "$plug_dir already exist."
     fi
+    ln -sfv $plug_dir $ohmyz_dir/custom/plugins/
+}
+
+newtheme ()
+{
+    theme_url=$1
+    theme_name=${theme_url##*/}
+    theme_dir=$zsh_dir/themes/$theme_name
+    if [ ! -d $theme_dir ]; then
+        git clone $theme_url $theme_dir
+    else
+        echo "$theme_dir already exist."
+    fi
+    ln -sfv $theme_dir/*zsh-theme $ohmyz_dir/custom/themes/
 }
 
 ohmyzsh ()
@@ -31,9 +44,9 @@ ohmyzsh ()
     [ ! -d $ohmyz_dir ] && git clone $ohmyz_url $ohmyz_dir 
 
     ## Plugins
-    newclone https://github.com/esc/conda-zsh-completion
-    newclone https://github.com/zsh-users/zsh-autosuggestions
-    ln -sfv $zsh_dir/plugins/* $ohmyz_dir/custom/plugins/
+    newplug https://github.com/esc/conda-zsh-completion
+    newplug https://github.com/zsh-users/zsh-autosuggestions
+    newtheme https://github.com/spaceship-prompt/spaceship-prompt
 }
 
 while true; do
