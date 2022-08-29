4f4975e614002be59291c34e9e5eab2537c16b28
rafiyq <rafiyq@protonmail.com>
2022-08-29 17:07:44 +0700

    fix toggle theme

--- toogle-themes.sh
+++ toogle-themes.sh
@@ -1,23 +1,21 @@
-theme_one=$1
-theme_two=$2
-theme_alt= echo $1 | sed -e "s/-light//" -e "s/-dark//"
-current_shell_theme=$(gsettings get org.gnome.shell.extensions.user-theme name | tr -d "'")
+#!/bin/sh
+
+light_gtk_theme="Prof-Gnome-Light-DS-3.6"
+dark_gtk_theme="Prof-Gnome-Dark-3.6"
+
+light_shell_theme="Prof-Gnome-Light-DS-3.6"
+dark_shell_theme="Prof-Gnome-Dark-3.6"
+
+#theme_alt=$(echo $1 | sed -e "s/-light//" -e "s/-dark//")
 current_gtk_theme=$(gsettings get org.gnome.desktop.interface gtk-theme | tr -d "'")
+current_shell_theme=$(gsettings get org.gnome.shell.extensions.user-theme name | tr -d "'")
 
-# toogle gtk theme
-if [ $theme_one = $current_gtk_theme ]
+if [ $light_gtk_theme = "$current_gtk_theme" ]
 then
-    gsettings set org.gnome.desktop.interface gtk-theme $theme_two
+    gsettings set org.gnome.desktop.interface gtk-theme $dark_gtk_theme
+    gsettings set org.gnome.shell.extensions.user-theme name $dark_shell_theme
 else
-    gsettings set org.gnome.desktop.interface gtk-theme $theme_one
+    gsettings set org.gnome.desktop.interface gtk-theme $light_gtk_theme
+    gsettings set org.gnome.shell.extensions.user-theme name $light_shell_theme
 fi
 
-# toogle shell theme
-if [ $theme_one = $current_shell_theme ]
-then
-    gsettings set org.gnome.shell.extensions.user-theme name "$theme_alt"
-    gsettings set org.gnome.shell.extensions.user-theme name $theme_two
-else
-    gsettings set org.gnome.shell.extensions.user-theme name "$theme_alt"
-    gsettings set org.gnome.shell.extensions.user-theme name $theme_one
-fi
