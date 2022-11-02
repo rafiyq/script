f449c2763dd5976fe16972c1c75217daa40ea18f
rafiyq <rafiyq@protonmail.com>
2022-11-02 16:03:02 +0700

    change vscode profile location

--- vscode.sh
+++ vscode.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-code_profiles_dir=~/.config/code_profile
+code_profiles_dir=~/.config/code_profiles
 
 start_vscode() {
     ext_dir=$code_profiles_dir/$lang/exts
     [ ! -d "$dir" ] && mkdir -pv "$ext_dir"
     data_dir=$code_profiles_dir/$lang/data
     [ ! -d "$dir" ] && mkdir -pv "$data_dir"
-    code --extensions-dir $ext_dir --user-data-dir $data_dir $2
-    echo "$ext_dir \n $data_dir"
+    code --extensions-dir $ext_dir --user-data-dir $data_dir $1
 }
 
 
@@ -16,22 +15,22 @@ case $1 in
     "cpp")
         lang="CPP"
         echo $lang
-        start_vscode
+        start_vscode $2
         ;;
     "js")
         lang="JavaScript"
         echo $lang
-        start_vscode
+        start_vscode $2
         ;;
     "py")
         lang="Python"
         echo $lang
-        start_vscode
+        start_vscode $2
         ;;
     "rs"|"rust")
         lang="Rust"
         echo $lang
-        start_vscode
+        start_vscode $2
         ;;
     *)
         echo "Unknown Option"; exit 1;
